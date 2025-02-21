65d44f83ab36352c6806ed0e8207b204c93fa264
LucasBBrando <lucasbonfim051@gmail.com>
2025-02-21 15:12:21 -0300

    Update xadrez.c
    Inclusão dos códigos.

--- xadrez.c
+++ xadrez.c
@@ -4,7 +4,7 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
+//int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
@@ -28,5 +28,61 @@ int main() {
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
 
-    return 0;
+//    return 0;
+//}
+void bispoMove(int walkBispo){
+    int upBispo = 0;
+    int dBispo = 0;
+    if(walkBispo > 0){
+        for(int x= 0; x < 1; x++){
+            printf("Cima|^| ");
+            for(int z = 0; z < 1; z++){
+                printf("Direita=>\n");
+            }
+        }
+        upBispo + 1;
+        dBispo + 1; 
+        bispoMove(walkBispo - 1);
+    }
+}
+
+
+int main(){
+    
+    //Movimento do Bispo
+    printf("Movimento da Bispo:\n");
+    int passBispo = 5;
+    bispoMove(passBispo);
+    
+    printf("\n");
+
+    //Movimento da Torre
+    printf("Movimento da Torre:\n");
+    for(int T = 0; T < 5; T++){
+        printf("Direita =>\n");
+    }
+
+    printf("\n");
+
+    //Movimento da Rainha
+    printf("Movimento da Rainha:\n");
+    for(int R = 0; R < 8; R++){
+        printf("Esquerda<=\n");
+    }
+    
+    printf("\n");
+
+    //Movimento do Cavalo
+    printf("Movimento da Cavalo:\n");
+    for(int H = 1; H < 4; H++){
+        printf("Cima|^|\n");
+        if(H == 3){
+            do{
+                printf("Direita=>\n");
+                break;
+            }while(H <= 3);
+            
+        }
+    }
+
 }
